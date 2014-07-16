58d466d3e1a601fd42eb41688697b8740b29adf0
AndyHu <meeasyhappy@gmail.com>
2014-07-16 18:33:35 +0800

    No message.

--- lib/generators/showbuilder/scaffold/scaffold_generator.rb
+++ lib/generators/showbuilder/scaffold/scaffold_generator.rb
@@ -4,11 +4,19 @@ require 'rails/generators/rails/scaffold_controller/scaffold_controller_generato
 module Showbuilder
   module Generators
     class ScaffoldGenerator < Rails::Generators::ResourceGenerator
+      source_root File.expand_path(File.join('..', 'templates'), __FILE__)
+
+      argument :test, :type => :string, :default => "skip-test-framework"
       remove_hook_for :resource_controller
+
       remove_class_option :actions
 
       class_option :slim, :default => true
-      hook_for     :slim,     :in => :showbuilder
+      hook_for     :slim,                    :in => :showbuilder
+    
+      def create_controller_file
+        template 'controller.rb', File.join('app/controllers', class_path, "#{plural_name}_controller.rb")
+      end
     end
   end
 end
