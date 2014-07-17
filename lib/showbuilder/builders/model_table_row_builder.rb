bdf72ad3b62c7586e34756f7d3ef34a23bad5a9b
AndyHu <meeasyhappy@gmail.com>
2014-07-17 18:04:51 +0800

    No message.

--- lib/showbuilder/builders/model_table_row_builder.rb
+++ lib/showbuilder/builders/model_table_row_builder.rb
@@ -116,6 +116,18 @@ module Showbuilder
         end
       end
 
+      #
+      def show_delete_link_column(*methods)
+        return show_header_column(methods) if is_header
+
+        content = get_methods_text_value(model, methods)
+        link_path = get_link_option(methods)
+
+        content_tag :td do
+          link_to content, link_path, :method => :delete
+        end
+      end
+
       private
 
       def show_header_column(methods = nil)
